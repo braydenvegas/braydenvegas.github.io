9ac8e18413e03001f70b7450f5d1902ca2dd6505
braydenvegas <brayden.vegas@jpschools.org>
2026-09-09 17:46:14 +0000

    completed circularity project

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -33,11 +33,11 @@ circles.push(circle);
 
 
         // TODO 3 : Call the drawCircle() function
-drawCircle();
-drawCircle();
-drawCircle();
-drawCircle();
-drawCircle();
+//drawCircle();
+//drawCircle();
+//drawCircle();
+//drawCircle();
+//drawCircle();
 
         // TODO 7 : Use a loop to create multiple circles
 for (var i = 0; i < 50; i++) {
